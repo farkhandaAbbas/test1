e9444e2545f80fc9f032196466d836297d8b76c8
farkhandaabbas0427@gmail.com <farkhandaabbas0427@gmail.com>
2019-01-05 19:32:54 +0500

    message

---
+++ secure/smtp-cred.js
@@ -0,0 +1,3 @@
+var get = { username : "farkhandaabbas0427@gmail.com", password : "farkhanda1234", fbID : "336741213831358", fbSecret : "53dc9002990a1cdeda32736ed2a5e50d", gID : "891308093159-okdiaogpjkbiecjeurrh5srqrflje05o.apps.googleusercontent.com", gSecret : "3U-24w_wgcAuKqU5caZnToFS" };
+
+module.exports = get;
